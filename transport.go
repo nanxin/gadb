ababbd2dc6cb7f4ce849ff1c0b34400b4a1ca8ac
xiao.zhang <xiao.zhang@unity.cn>
2023-03-17 13:29:58 +0800

    change DefaultAdbReadTimeout to 120s

--- transport.go
+++ transport.go
@@ -12,7 +12,7 @@ import (
 
 var ErrConnBroken = errors.New("socket connection broken")
 
-var DefaultAdbReadTimeout time.Duration = 60
+var DefaultAdbReadTimeout time.Duration = 120
 
 type transport struct {
 	sock        net.Conn
